be7e7b05559c90b4d9e91fc33f915df7b2a5afe3
Antti Leppä <antti.leppa@metatavu.fi>
2018-03-05 14:40:09 +0200

    Changed all but front page to require logged user

--- functions.php
+++ functions.php
@@ -3,6 +3,12 @@
   require_once( __DIR__ . '/shortcodes.php');
   require_once( __DIR__ . '/customizer.php');
 
+  add_action('wp', function () {
+    if(!is_front_page() && !is_user_logged_in()) {
+      auth_redirect();
+    }
+  });
+
   add_action( 'wp_enqueue_scripts', function () {
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css' );
     wp_dequeue_style('wp-bootstrap-starter-style');
